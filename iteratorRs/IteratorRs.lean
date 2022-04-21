5610140ec9dba063fabb5a2c1cb2c3a2e83c2dbb
Adrien Champion <AdrienChampion@users.noreply.github.com>
2022-04-21 23:20:40 +0200

    added more content on iterators

--- iteratorRs/IteratorRs.lean
+++ iteratorRs/IteratorRs.lean
@@ -25,17 +25,40 @@ class FromIter (Tgt : Type u → Type v) where
 
 
 
+partial def Iter.fold
+  [Iter I]
+  (iter : I)
+  (init : Out)
+  (step : Out → Iter.Item I → Out)
+  : Out
+:=
+  match next iter with
+  | none =>
+    init
+  | some (elm, iter) =>
+    fold iter (step init elm) step
+
+def Iter.collect
+  [Iter I]
+  [FromIter Tgt]
+  (iter : I)
+  : Tgt (Iter.Item I)
+:=
+  FromIter.fromIter iter
+
+
+
 section listIter
-  partial def List.fromIter
+  partial def List.collect
     [Iter I] (iter : I)
     : List (Iter.Item I)
   :=
     match Iter.next iter with
     | none => []
-    | some (elm, iter) => elm::(fromIter iter)
+    | some (elm, iter) => elm::(collect iter)
 
   instance ListFromIter : FromIter List where
-    fromIter := List.fromIter
+    fromIter := List.collect
 
   structure List.Iterator (α : Type u) where
     list : List α
@@ -48,27 +71,27 @@ section listIter
       | [] => none
       | next::tail => some (next, List.Iterator.mk tail)
 
-  def List.intoIter (self : List α) : Iterator α :=
+  def List.iter (self : List α) : Iterator α :=
     Iterator.mk self
 
   instance ListIntoIter : IntoIter (List α) where
     IntoIter := List.Iterator α
     IntoIterIsIter := inferInstance
-    intoIter self := self.intoIter
+    intoIter self := self.iter
 end listIter
 
 
 
 section arrayIter
-  --- An array is just a list, let's re-use `List.fromIter`.
-  partial def Array.fromIter
+  --- An array is just a list, let's re-use `List.collect`.
+  partial def Array.collect
     [Iter I] (iter : I)
     : Array (Iter.Item I)
   :=
-    List.fromIter iter |> Array.mk
+    List.collect iter |> Array.mk
 
   instance ArrayFromIter : FromIter Array where
-    fromIter := Array.fromIter
+    fromIter := Array.collect
 
   --- Again, an array is actually just a `data` field of type `List α`, so we could just user
   --- `List.Iterator`. This version is actually much worse performance-wise, because of all the
@@ -92,13 +115,13 @@ section arrayIter
       else
         none
 
-  def Array.intoIter (self : Array α) : Iterator α :=
+  def Array.iter (self : Array α) : Iterator α :=
     Iterator.new self
 
   instance ArrayIntoIter : IntoIter (Array α) where
     IntoIter := Array.Iterator α
     IntoIterIsIter := inferInstance
-    intoIter self := self.intoIter
+    intoIter self := self.iter
 end arrayIter
 
 
@@ -117,6 +140,26 @@ section combineIter
     (subIter : I) (filterMap : Iter.Item I → Option Out)
     : FilterMap I Out
   :=
+    { filterMap, subIter }
+  
+  def Iter.filter
+    [Iter I]
+    (subIter : I) (filter : Iter.Item I → Bool)
+    : FilterMap I (Iter.Item I)
+  :=
+    let filterMap item :=
+      if filter item
+      then some item
+      else none
+    { filterMap, subIter }
+  
+  def Iter.map
+    [Iter I]
+    (subIter : I) (map : Iter.Item I → Out)
+    : FilterMap I Out
+  :=
+    let filterMap item :=
+      map item |> some
     { filterMap, subIter }
 
   private partial def Iter.FilterMap.innerNext
@@ -163,26 +206,28 @@ end combineIter
 
 section dumbExamples
 
+  --- Removes the odd naturals in a list.
   def List.filterEvenToArrayString
     (self : List Nat)
     : Array String
   :=
-    Iter.filterMap self.intoIter (
+    Iter.filterMap self.iter (
       fun (n : Nat) =>
         if n % 2 = 0 then toString n |> some else none
     )
-    |> Array.fromIter
+    |> Iter.collect
 
+  --- Removes the even naturals in a list.
   def List.filterOddToArrayString
     (self : List Nat)
     : Array String
   :=
-    Iter.filterMap self.intoIter (
+    Iter.filterMap self.iter (
       fun (n : Nat) =>
         if n % 2 = 1 then toString n |> some else none
     )
-    |> Array.fromIter
-  
+    |> Iter.collect
+
   def list : List Nat := [0, 1, 2, 3, 4, 5, 6]
 
   #eval list.filterEvenToArrayString
@@ -191,12 +236,30 @@ section dumbExamples
   def merged : List (String × String) :=
     let even :=
       list.filterEvenToArrayString
-      |> Array.intoIter
+      |> Array.iter
     let odd :=
       list.filterOddToArrayString
-      |> Array.intoIter
+      |> Array.iter
     Iter.zip even odd
-    |> List.fromIter
+    |> Iter.collect
   #eval merged
+
+  def joined : List String :=
+    Iter.map
+      merged.iter
+      (fun (lft, rgt) => s! "{lft} × {rgt}")
+    |> List.collect
+  #eval joined
+
+  def folded : String :=
+    Iter.fold joined.iter "" (
+      fun s (elm : String) =>
+        let sep :=
+          if s.length = 0
+          then ""
+          else ", "
+        s! "{s}{sep}{elm}"
+    )
+  #eval folded
 
 end dumbExamples
