ec2826cac2f2b5dc549acc3a1dfe4d0c2e11bd53
Adrien Champion <AdrienChampion@users.noreply.github.com>
2022-05-14 01:29:26 +0200

    mon category

--- category/Category.lean
+++ category/Category.lean
@@ -108,7 +108,6 @@ instance Cat.CSet : Cat CSet.All where
 
 namespace Poset
 
-  @[reducible]
   structure All where
     Elm : α
     ord : PartialOrder Elm
@@ -180,3 +179,85 @@ instance Cat.Poset : Cat Poset.All where
     @Poset.ProperFn.compose_nop
   nop_compose :=
     @Poset.ProperFn.nop_compose
+
+
+
+namespace Mon
+
+  structure All where
+    Elm : α
+    mon : Monoid α
+
+  def All.mul {self : All} (a b : self.Elm) : self.Elm :=
+    self.mon.mul a b
+  
+  local infixl:70 " * " => All.mul
+
+
+
+  structure Homo (m₁ m₂ : All) where
+    apply (a₁ : m₁.Elm) : m₂.Elm
+    apply_one : apply m₁.mon.one = m₂.mon.one
+    apply_proper (a₁ b₁ : m₁.Elm) :
+      apply (a₁ * b₁) = apply a₁ * apply b₁
+
+  def Homo.compose
+    (g : Homo m₂ m₃)
+    (f : Homo m₁ m₂)
+    : Homo m₁ m₃
+  where
+    apply a₁ := g.apply (f.apply a₁)
+    apply_one :=
+      by
+        simp
+        rw [f.apply_one, g.apply_one]
+    apply_proper a₁ b₁ :=
+      by
+        simp
+        rw [f.apply_proper, g.apply_proper]
+
+  def Homo.compose_assoc
+    (h : Homo s₃ s₄)
+    (g : Homo s₂ s₃)
+    (f : Homo s₁ s₂)
+    : h.compose (g.compose f) = (h.compose g).compose f
+  :=
+    rfl
+
+
+
+  def Homo.nop {s : outParam All} : Homo s s where
+    apply := id
+    apply_one := rfl
+    apply_proper _ _ := rfl
+
+  theorem Homo.compose_nop
+    (f : Homo m₁ m₂)
+    : f.compose nop = f
+  :=
+    rfl
+
+  theorem Homo.nop_compose
+    (f : Homo m₁ m₂)
+    : nop.compose f = f
+  :=
+    rfl
+
+end Mon
+
+instance Cat.Mon : Cat Mon.All where
+  Arrow :=
+    Mon.Homo
+
+  compose :=
+    Mon.Homo.compose
+  compose_assoc :=
+    Mon.Homo.compose_assoc
+
+
+  nop :=
+    @Mon.Homo.nop
+  nop_compose :=
+    Mon.Homo.nop_compose
+  compose_nop :=
+    Mon.Homo.compose_nop
